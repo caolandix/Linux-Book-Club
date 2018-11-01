fcd0fd2a4f033264ce5efe5902746525b554fe0f
ODomhnaill, Caolan <caolan@aeshna.net>
2018-11-01 10:09:58 -0700

    Cleaned up code

--- Assignment2.sh
+++ Assignment2.sh
@@ -4,11 +4,10 @@ echo 'Date: Mon Oct 29 14:55:47 PDT 2018'
 echo "Assignment #2 - loads the robots.txt from many different domains and calculates some stats"
 echo "The purpose of this script is to download the opendns top domains, and attempt to curl each of their robots.txt files."
 echo "Then it finds the average robots.txt file, and output the top 5 user-agents and disallowed"
+echo "Parameter #1 is the number of processor items that xargs uses to execute"
 
 url_top_domains="https://raw.githubusercontent.com/opendns/public-domain-lists/master/opendns-top-domains.txt"
 file_top_domains="opendns-top-domains.txt"
-file_size_sum=0
-num_usable_domain_files=0
 
 list_of_outputs=("user-agent.txt" "disallow.txt")
 
@@ -24,10 +23,7 @@ done
 echo "cURLing the file ${url_top_domains}"
 curl -o ${file_top_domains} -L --connect-timeout 60 ${url_top_domains}
 
-file_top_domains="opendns-top-domains.txt"
-
-# xargs -a ${file_top_domains} -d "\n" -P 4 -I{} ./script.sh {}
-xargs -r -a ${file_top_domains} -d "\n" -P 1024 -I{} ./script.sh {}
+xargs -r -a ${file_top_domains} -d "\n" -P $1 -I{} ./script.sh {}
 
 sleep 3s
 echo "The Top 5 user Agents: "
